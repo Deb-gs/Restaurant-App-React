be24557146b35e1d2bb3a08c6ee6b37f7f4dcdc0
Deb-gs <ghoshdev53@gmail.com>
2022-03-22 06:33:55 +0530

    my first commit reactwebsite

---
+++ src/Components/Basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet.",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Aloo pakoda are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks. Make this awesome vegan potato snack in less than 30 minutes.",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Corn is one of the most widely consumed cereal grains. As a good source of antioxidant carotenoids, such as lutein and zeaxanthin, yellow corn may promote eye health. It's also a rich source of many vitamins and minerals.",
+    },
+    {
+      id: 4,
+      image: "../images/biryani.jpg",
+      name: "Biriyani",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "This biryani has mild fragrance, sticky rice, distinct taste of rice with hot and tangy flavour along with soft meat or vegetables. Use TasteMeter language to universally describe the unique taste of different biryanis.",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Pizza tastes like bread tomato and cheese, chewy, moist, slightly acidic, and sharp. Then once you apply toppings, that changes it. Ham and pineapple, adds salty, sweet and juicy. Pepperoni adds a meatiness and spice.",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Thali (meaning \"plate\") or Bhojanam (meaning \"full meal\") is a round platter used to serve food in South Asia, Southeast Asia and the Caribbean. Thali is also used to refer to an Indian-style meal made up of a selection of various dishes which are served on a platter.",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "You know Bengalis have a sweet tooth, and you know that they have created some of the best desserts you can imagine. Their history boasts of sweets made traditionally during festivals and special occasions--from nolen gurer sondesh to stuffed pithas",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma Rice tastes slightly sour, spicy and hot with bursting flavors of spices. A well-made rasam has the right balance or sour, sweet and hot tastes.",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Piercing the outer shell, you inhale the aroma and pause. You eat a piece, as the blended taste of brittle crust and soft potatoes laced with caraway, coriander and chillies dissolves.",
+    },
+  ];
+  
+  export default Menu;
